9e546c3407fd771889341d18d7c7f36454e72ca9
JRaaz <jonnyramos10@gmail.com>
2020-03-30 22:55:35 -0300

    Testes da Aplicação

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -14,7 +14,7 @@ export default function Detail() {
 
   const incident = route.params.incident;
 
-  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}`;
+  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}.`;
 
   function navigateBack() {
     navigation.goBack();
@@ -29,7 +29,7 @@ export default function Detail() {
   }
 
   function sendWhatsapp() {
-    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`);
+    Linking.openURL(`whatsapp://send?phone=55${incident.whatsapp}&text=${message}`);
   }
 
   return (
